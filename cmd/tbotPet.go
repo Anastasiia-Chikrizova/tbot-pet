9c87f2df9a19a50ffdf1ccf865e4f09f4a6166e0
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2025-07-24 21:08:19 +0300

    Add basic Telegram bot support with Telebot library

--- cmd/tbotPet.go
+++ cmd/tbotPet.go
@@ -1,19 +1,28 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v4"
+)
+
+var (
+	// TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // tbotPetCmd represents the tbotPet command
 var tbotPetCmd = &cobra.Command{
-	Use:   "tbotPet",
-	Short: "A brief description of your command",
+	Use:     "tbot-pet",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +30,31 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("tbotPet called")
+		fmt.Printf("tbot-pet %s started\n", appVersion)
+
+
+		tbotPet, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+		}
+		tbotPet.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Printf("Received message: %s", m.Text())
+			payload := m.Message().Payload
+
+           switch payload {
+           			case "hello":
+           				err = m.Send(fmt.Sprintf("Hello I'm Tbot-pet %s!", appVersion))
+
+           				}
+           return err
+	    })
+
+		tbotPet.Start()
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/anastasiia-chikrizova/tbot-pet
 go 1.23.6
 
 require (
+	github.com/spf13/cobra v1.9.1
+	gopkg.in/telebot.v4 v4.0.0-beta.5
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 )
